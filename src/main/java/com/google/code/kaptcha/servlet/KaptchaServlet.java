0386f96bccb949b508adb84ed5abe196033b96e8
Alexey Kuznetsov <axet@me.com>
2013-07-24 20:51:47 +0400

    Fix Issue #4

--- src/main/java/com/google/code/kaptcha/servlet/KaptchaServlet.java
+++ src/main/java/com/google/code/kaptcha/servlet/KaptchaServlet.java
@@ -3,19 +3,31 @@ package com.google.code.kaptcha.servlet;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.Properties;
 
 import javax.imageio.ImageIO;
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.google.code.kaptcha.Producer;
 import com.google.code.kaptcha.util.Config;
 
-public class KaptchaExtend {
+/**
+ * This servlet uses the settings passed into it via the Producer api.
+ *
+ * @author testvoogd@hotmail.com
+ * @author jon
+ * @author cliffano
+ */
+@SuppressWarnings("serial")
+public class KaptchaServlet extends HttpServlet implements Servlet
+{
 	private Properties props = new Properties();
 
 	private Producer kaptchaProducer = null;
@@ -24,13 +36,26 @@ public class KaptchaExtend {
 
 	private String sessionKeyDateValue = null;
 
-	public KaptchaExtend() {
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see javax.servlet.Servlet#init(javax.servlet.ServletConfig)
+	 */
+	@Override
+	public void init(ServletConfig conf) throws ServletException
+	{
+		super.init(conf);
+
 		// Switch off disk based caching.
 		ImageIO.setUseCache(false);
 
-		this.props.put("kaptcha.border", "no");
-		this.props.put("kaptcha.textproducer.font.color", "black");
-		this.props.put("kaptcha.textproducer.char.space", "5");
+		Enumeration<?> initParams = conf.getInitParameterNames();
+		while (initParams.hasMoreElements())
+		{
+			String key = (String) initParams.nextElement();
+			String value = conf.getInitParameter(key);
+			this.props.put(key, value);
+		}
 
 		Config config = new Config(this.props);
 		this.kaptchaProducer = config.getProducerImpl();
@@ -38,16 +63,11 @@ public class KaptchaExtend {
 		this.sessionKeyDateValue = config.getSessionDate();
 	}
 
-	/**
-	 * map it to the /url/captcha.jpg
-	 * 
-	 * @param req
-	 * @param resp
-	 * @throws ServletException
-	 * @throws IOException
-	 */
-	public void captcha(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	/** */
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException
+	{
 		// Set standard HTTP/1.1 no-cache headers.
 		resp.setHeader("Cache-Control", "no-store, no-cache");
 
@@ -57,14 +77,6 @@ public class KaptchaExtend {
 		// create the text for the image
 		String capText = this.kaptchaProducer.createText();
 
-		// store the text in the session
-		req.getSession().setAttribute(this.sessionKeyValue, capText);
-
-		// store the date in the session so that it can be compared
-		// against to make sure someone hasn't taken too long to enter
-		// their kaptcha
-		req.getSession().setAttribute(this.sessionKeyDateValue, new Date());
-
 		// create the image with the text
 		BufferedImage bi = this.kaptchaProducer.createImage(capText);
 
@@ -73,8 +85,7 @@ public class KaptchaExtend {
 		// write the data out
 		ImageIO.write(bi, "jpg", out);
 
-		// fixes issue #69: set the attributes after we write the image in case
-		// the image writing fails.
+		// fixes issue #69: set the attributes after we write the image in case the image writing fails.
 
 		// store the text in the session
 		req.getSession().setAttribute(this.sessionKeyValue, capText);
@@ -83,11 +94,5 @@ public class KaptchaExtend {
 		// against to make sure someone hasn't taken too long to enter
 		// their kaptcha
 		req.getSession().setAttribute(this.sessionKeyDateValue, new Date());
-	}
-
-	public String getGeneratedKey(HttpServletRequest req) {
-		HttpSession session = req.getSession();
-		return (String) session
-				.getAttribute(com.google.code.kaptcha.Constants.KAPTCHA_SESSION_KEY);
 	}
 }
